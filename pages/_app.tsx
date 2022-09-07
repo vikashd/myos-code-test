09ef14b8e4d742efb5ff686c683bc65086d79fd0
Vikash Deepchand <vikash.deepchand@gmail.com>
2022-09-07 13:12:50 +0700

    Disable zooming

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,23 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Orders } from "../state/orders/Orders";
 import { Products } from "../state/products/Products";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Orders>
-    <Products>
-      <Component {...pageProps} />
-    </Products>
-  </Orders>
+  <>
+    <Head>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+      />
+    </Head>
+    <Orders>
+      <Products>
+        <Component {...pageProps} />
+      </Products>
+    </Orders>
+  </>
 );
 
 export default MyApp;
